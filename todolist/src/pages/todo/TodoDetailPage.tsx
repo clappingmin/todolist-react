5beb0db16a4c0c1922aaf9a53b13e317503bf4a4
Sumin Park <clappingmin@gmail.com>
2023-09-26 14:54:31 +0900

    투두리스트 상세 페이지 jotai 이용해서 상태관리 (#2)
    * feat: firebase todo id, order 저장, order 정렬해서 가져오기
    
    * feat: todo 완료하기 기능 개발
    
    * refactor: todos update, get 서버요청 영역 조타이에서 수행하게 수정
    
    * feat: 특정 투두 데이터 가져오기
    
    * feat: 투두 디테일 페이지에서 데이터 가져오기
    
    * docs: pr 탬플릿 추가

--- todolist/src/pages/todo/TodoDetailPage.tsx
+++ todolist/src/pages/todo/TodoDetailPage.tsx
@@ -1,8 +1,25 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { Spinner } from '@chakra-ui/react';
+import { findTodo } from '../../store/todo';
+import { useEffect, useMemo } from 'react';
+
 function TodoDetailPage() {
+  const navigate = useNavigate();
+  const { todoId } = useParams();
+  const todo = todoId ? findTodo(todoId) : null;
+
+  // todo todo 뒤로보내기
+
   return (
-    <>
-      <h1>디테일 화면</h1>
-    </>
+    <div>
+      {todo ? (
+        <>
+          <div>{todo.id}</div>
+        </>
+      ) : (
+        <div>없음</div>
+      )}
+    </div>
   );
 }
 
---
+++ todolist/src/store/todo.ts
@@ -0,0 +1,41 @@
+import { atom, useAtom, useSetAtom } from 'jotai';
+import { getTodosFB, updateTodoFB } from '../service/firebase';
+import { Todo } from '../shared/interfaces/todo.interface';
+
+// 정의
+export const todosAtom = atom<Array<Todo>>([]);
+
+// 파생된 atom - todos의 길이
+export const todosLenAtom = atom((get) => get(todosAtom).length);
+
+// 원하는 투두 하나 찾기
+export const findTodo = (todoId: string) => {
+  useSetAtom(setFBTodosAtom)();
+
+  return useAtom(todosAtom)[0].find((todo) => todo.id === todoId);
+};
+
+// 서버에서 전체 todo 가져오기
+export const setFBTodosAtom = atom(null, async (get, set) => {
+  if (!get(todosLenAtom)) {
+    const newTodos: Array<Todo> = await getTodosFB();
+    set(todosAtom, newTodos);
+  }
+});
+
+// 추가하기
+export const addTodoAtom = atom(null, (get, set, newTodo: Todo) => {
+  set(todosAtom, [...get(todosAtom), newTodo]);
+});
+
+// 수정하기
+export const updateTodoAtom = atom(null, (get, set, updateTodo: Todo) => {
+  const updatedtodos = get(todosAtom).map((todo) => {
+    if (todo.id === updateTodo.id) return updateTodo;
+    return todo;
+  });
+
+  updateTodoFB(updateTodo);
+
+  set(todosAtom, updatedtodos);
+});
